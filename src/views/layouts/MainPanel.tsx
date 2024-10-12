f0dffb165cb894d59ce6f3860ac0cc6cc7a186f8
SiHoon61 <dlatlgns000@gmail.com>
2024-10-12 13:19:07 +0000

    add: home 버튼

--- src/views/layouts/MainPanel.tsx
+++ src/views/layouts/MainPanel.tsx
@@ -1,11 +1,21 @@
 import { css } from '@emotion/react';
-import { Outlet } from 'react-router-dom';
+import { HomeOutlined } from '@ant-design/icons';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 const containerCss = css`
   display: flex;
   flex-direction: column;
   width: 100%;
   height: 100%;
+  position: relative;
+`;
+
+const homeIconCss = css`
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-size: 24px;
+  z-index: 1000;
 `;
 
 const contentCss = css`
@@ -15,8 +25,15 @@ const contentCss = css`
 `;
 
 function MainPanel() {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate('/signin');
+  };
+
   return (
     <div css={containerCss}>
+      <HomeOutlined css={homeIconCss} onClick={handleHomeClick} />
       <div css={contentCss}>
         <Outlet />
       </div>
