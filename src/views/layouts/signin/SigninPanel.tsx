099960fc2dc8eae8845afa8f950e73228555fc58
SiHoon61 <dlatlgns000@gmail.com>
2025-01-05 09:57:56 +0000

    [수정] 소켓 코드 주석처리

--- src/views/layouts/signin/SigninPanel.tsx
+++ src/views/layouts/signin/SigninPanel.tsx
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import { Button, Form, Input } from 'antd';
+import { useState, useRef } from 'react';
 import { requestSignin } from 'api/requests/requestAuth';
 import { useNavigate } from 'react-router-dom';
 import { colorLight } from 'styles/colors';
@@ -10,6 +11,7 @@ import { transformCoinData } from 'views/CoinConverter';
 import { useCoinListStore, useUserClickStreamStore } from 'stores/userInfoStore';
 import { useUserAnalysisStore } from 'stores/userInfoStore';
 import { getUserAnalysis } from 'api/requests/requestUserData';
+const websocketURL = import.meta.env.VITE_WEBSOCKET_API;
 
 const containerCss = css`
   width: 100%;
@@ -107,52 +109,127 @@ function SigninPanel() {
     navigate('/');
   };
 
+  // const [output, setOutput] = useState<string[]>([]);
+  // const socketRef = useRef<WebSocket | null>(null);
+
+  // const logOutput = (message: string) => {
+  //   setOutput((prev) => [...prev, message]);
+  // };
+
+  // const connectWebSocket = () => {
+  //   const url = websocketURL;
+
+  //   // WebSocket 객체 생성
+  //   const socket = new WebSocket(url);
+  //   socketRef.current = socket;
+
+  //   socket.onopen = () => logOutput('WebSocket 연결 성공!');
+  //   socket.onmessage = (event) => logOutput(`수신 데이터: ${event.data}`);
+  //   socket.onclose = () => logOutput('WebSocket 연결 종료');
+  //   socket.onerror = (error) => logOutput(`에러 발생: ${error}`);
+  // };
+
+  // const disconnectWebSocket = () => {
+  //   if (socketRef.current) {
+  //     socketRef.current.close();
+  //     socketRef.current = null;
+  //   }
+  // };
+
+  // const sendRequest = () => {
+  //   if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+  //     const message = JSON.stringify({ action: 'getData' }); // Lambda에서 처리되는 액션 이름
+  //     socketRef.current.send(message);
+  //     logOutput('데이터 요청 전송 완료');
+  //   } else {
+  //     logOutput('WebSocket 연결이 없습니다.');
+  //   }
+  // };
+
+  // const stopRequest = () => {
+  //   if (socketRef.current && socketRef.current.readyState === WebSocket.OPEN) {
+  //     const message = JSON.stringify({ action: 'stopData' }); // Lambda에서 처리되는 액션 이름
+  //     socketRef.current.send(message);
+  //     logOutput('데이터 요청 중지 전송 완료');
+  //   } else {
+  //     logOutput('WebSocket 연결이 없습니다.');
+  //   }
+  // };
+
   return (
-    <div css={containerCss}>
-      <div css={titleCss}>코인예측왕</div>
-      <Form onFinish={onFinish}>
-        <Form.Item<FieldType>
-          name="name"
-          rules={[{ required: true, message: '닉네임을 입력해주세요.' }]}
-        >
-          <Input css={inputCss} placeholder="닉네임" />
-        </Form.Item>
-        <Form.Item<FieldType>
-          name="affiliation"
-          rules={[{ required: true, message: '소속을 입력해주세요.' }]}
-        >
-          <Input css={inputCss} placeholder="소속" />
-        </Form.Item>
-        {/* <Form.Item<FieldType>
+    <>
+      {/* <div>
+        <h2>WebSocket 테스트</h2>
+        <button onClick={connectWebSocket}>연결</button>
+        <button onClick={disconnectWebSocket}>연결 해제</button>
+        <button onClick={sendRequest}>데이터 요청</button>
+        <button onClick={stopRequest}>데이터 요청 중지</button>
+        <pre>{output.join('\n')}</pre>
+      </div> */}
+      <div css={containerCss}>
+        <div css={titleCss}>코인예측왕</div>
+        <Form onFinish={onFinish}>
+          <Form.Item<FieldType>
+            name="name"
+            rules={[{ required: true, message: '닉네임을 입력해주세요.' }]}
+          >
+            <Input css={inputCss} placeholder="닉네임" />
+          </Form.Item>
+          <Form.Item<FieldType>
+            name="affiliation"
+            rules={[{ required: true, message: '소속을 입력해주세요.' }]}
+          >
+            <Input css={inputCss} placeholder="소속" />
+          </Form.Item>
+          {/* <Form.Item<FieldType>
           name="student_id"
           rules={[{ required: true, message: '학번을 입력해주세요.' }]}
         >
           <Input css={inputCss} placeholder="학번" />
         </Form.Item> */}
-        <Form.Item<FieldType> name="nickname">
-          <Input css={inputCss} placeholder="이메일(선택항목)" />
-        </Form.Item>
-         <div>이메일은 경품 전달을 위해서만 사용됩니다</div>
-        <div css={btnContainerCss}>
-          <Button type="primary" htmlType="submit" css={startBtnCss}>
-            게임 시작
-          </Button>
-          <div
+          <Form.Item<FieldType> name="nickname">
+            <Input css={inputCss} placeholder="이메일(선택항목)" />
+          </Form.Item>
+          <div>이메일은 경품 전달을 위해서만 사용됩니다</div>
+          <div css={btnContainerCss}>
+            <Button type="primary" htmlType="submit" css={startBtnCss}>
+              게임 시작
+            </Button>
+            <div
+              css={css`
+                display: flex;
+                gap: 10px;
+              `}
+            >
+              <Button css={rankBtnCss} onClick={handleRank}>
+                랭킹 확인
+              </Button>
+              <Button css={rankBtnCss} onClick={handleAnalysis}>
+                통계 확인
+              </Button>
+            </div>
+          </div>
+        </Form>
+        <div
+          css={css`
+            position: absolute;
+            left: 10px;
+            bottom: 10px;
+          `}
+        >
+          <p
             css={css`
-              display: flex;
-              gap: 10px;
+              font-size: 14px;
             `}
           >
-            <Button css={rankBtnCss} onClick={handleRank}>
-              랭킹 확인
-            </Button>
-            <Button css={rankBtnCss} onClick={handleAnalysis}>
-              통계 확인
-            </Button>
-          </div>
+            Contact Us
+          </p>
+          FE: dlatlgns000@gmail.com
+          <br /> BE/DE: yih5025@gmail.com
+          <br /> AI: pomatoyeah@gmail.com
         </div>
-      </Form>
-    </div>
+      </div>
+    </>
   );
 }
 
